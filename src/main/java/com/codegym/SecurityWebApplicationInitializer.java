3e748a2129cfe0c25b1e191356b748c7cdb645ac
Kien Nguyen <kienkun1990@gmail.com>
2019-12-25 10:57:56 +0700

    security

---
+++ src/main/java/com/codegym/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.codegym;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/com/codegym/controller/MvcWebApplicationInitializer.java
@@ -0,0 +1,23 @@
+package com.codegym.controller;
+
+import com.codegym.WebConfig;
+import com.codegym.WebSecurityConfig;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebSecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
